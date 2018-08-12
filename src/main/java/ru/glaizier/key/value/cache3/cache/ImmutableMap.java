efd319c33f6d6758321086ceedd1e06b25c50deb
GlaIZier <kh.mv@bk.ru>
2018-08-12 11:15:57 +0300

    ImmutableMap has been added

---
+++ src/main/java/ru/glaizier/key/value/cache3/cache/ImmutableMap.java
@@ -0,0 +1,29 @@
+package ru.glaizier.key.value.cache3.cache;
+
+import java.util.Optional;
+
+/**
+ * @author GlaIZier
+ */
+public interface ImmutableMap<K, V> {
+
+    Optional<V> get(K key);
+
+    /**
+     * @return previous value or empty if there was no such value. Or return Optional.empty when no such key.
+     * Use contains() to handle this situation
+     */
+    Optional<V> remove(K key);
+
+    boolean contains(K key);
+
+    /**
+     * @return current number of elements
+     */
+    int getSize();
+
+    default boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+}
