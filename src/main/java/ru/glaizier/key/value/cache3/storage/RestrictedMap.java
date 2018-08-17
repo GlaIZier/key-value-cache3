64dc8d08ec4329e238f5196d113f1c60d78d35de
GlaIZier <kh.mv@bk.ru>
2018-08-17 11:26:15 +0300

    RestrictedMap has been added

--- src/main/java/ru/glaizier/key/value/cache3/storage/RestrictedMap.java
+++ src/main/java/ru/glaizier/key/value/cache3/storage/RestrictedMap.java
@@ -2,19 +2,21 @@ package ru.glaizier.key.value.cache3.storage;
 
 import java.util.Optional;
 
+import javax.annotation.Nonnull;
+
 /**
  * @author GlaIZier
  */
-public interface ImmutableMap<K, V> {
+public interface RestrictedMap<K, V> {
 
-    Optional<V> get(K key);
+    Optional<V> get(@Nonnull K key);
 
     /**
-     * @return previous value or Optional.empty if there was no such value.
+     * @return removed value or empty if the key was not found.
      */
-    Optional<V> remove(K key);
+    Optional<V> remove(@Nonnull K key);
 
-    boolean contains(K key);
+    boolean contains(@Nonnull K key);
 
     /**
      * @return current number of elements
