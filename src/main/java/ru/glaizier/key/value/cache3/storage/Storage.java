42d309f3c8e29889ab30484e45b4c39cef19c157
GlaIZier <kh.mv@bk.ru>
2018-08-23 12:35:31 +0300

    Storage has been added

---
+++ src/main/java/ru/glaizier/key/value/cache3/storage/Storage.java
@@ -0,0 +1,18 @@
+package ru.glaizier.key.value.cache3.storage;
+
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Interface for a key-value storage
+ * @author GlaIZier
+ */
+public interface Storage<K, V> extends RestrictedMap<K, V> {
+
+    /**
+     * @return previous value or empty if there was no such key before
+     */
+    Optional<V> put(@Nonnull K key, @Nonnull V value);
+
+}
