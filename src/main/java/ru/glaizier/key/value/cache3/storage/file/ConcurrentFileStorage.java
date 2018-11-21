6545a68c34558b744155283ff6a68e0b6ddb8f80
GlaIZier <kh.mv@bk.ru>
2018-11-21 12:48:19 +0300

    AbstractFileStorage has been added

--- src/main/java/ru/glaizier/key/value/cache3/storage/file/ConcurrentFileStorage.java
+++ src/main/java/ru/glaizier/key/value/cache3/storage/file/ConcurrentFileStorage.java
@@ -1,39 +1,21 @@
 package ru.glaizier.key.value.cache3.storage.file;
 
-import static java.lang.String.format;
 import static java.util.Optional.empty;
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toConcurrentMap;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.invoke.MethodHandles;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
-import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
-import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import ru.glaizier.key.value.cache3.storage.Storage;
 import ru.glaizier.key.value.cache3.storage.StorageException;
-import ru.glaizier.key.value.cache3.util.Entry;
 
 /**
  * Objects in the heap (locks map) are used to introduce flexible (partial) locking. We could introduce even more
@@ -42,56 +24,22 @@ import ru.glaizier.key.value.cache3.util.Entry;
 @ThreadSafe
 // We don't use local locks for locking (we use locks in the heap)
 @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
-public class ConcurrentFileStorage<K extends Serializable, V extends Serializable> implements Storage<K, V> {
+public class ConcurrentFileStorage<K extends Serializable, V extends Serializable> extends AbstractFileStorage<K, V> {
 
-    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-    // filename format: <keyHash>-<uuid>.ser
-    private static final String FILENAME_FORMAT = "%d#%s.ser";
-
-    private static final Path TEMP_FOLDER = Paths.get(System.getProperty("java.io.tmpdir")).resolve("key-value-cache3");
-
-    private static final Pattern FILENAME_PATTERN = Pattern.compile("^(\\d+)#(\\S+)\\.(ser)$");
-
-    private final ConcurrentMap<K, Path> contents;
-
+    // guards the folder and contents-disk invariant
     private final ConcurrentMap<K, Object> locks;
-
-    @GuardedBy("locks.get()")
-    private final Path folder;
 
     public ConcurrentFileStorage() {
         this(TEMP_FOLDER);
     }
 
-    public ConcurrentFileStorage(Path folder) {
-        Objects.requireNonNull(folder, "folder");
-        this.folder = folder;
+    public ConcurrentFileStorage(@Nonnull Path folder) {
+        super(folder);
         try {
-            if (Files.notExists(folder)) {
-                Files.createDirectories(folder);
-            }
-            contents = buildContents(folder);
             locks = buildLocks(contents.keySet());
         } catch (Exception e) {
             throw new StorageException(e.getMessage(), e);
         }
-    }
-
-    private ConcurrentMap<K, Path> buildContents(Path folder) throws IOException {
-        return Files.walk(folder)
-                .filter(Files::isRegularFile)
-                .filter(path -> FILENAME_PATTERN.matcher(path.getFileName().toString()).find())
-                .map(path -> {
-                    try {
-                        return new Entry<>(deserialize(path).key, path);
-                    } catch (Exception e) {
-                        log.error("Couldn't deserialize key-value for the path: " + path, e);
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
-                .collect(toConcurrentMap(entry -> entry.key, entry -> entry.value, (one, another) -> one));
     }
 
     private ConcurrentMap<K, Object> buildLocks(Set<K> keys) throws IOException {
@@ -102,6 +50,7 @@ public class ConcurrentFileStorage<K extends Serializable, V extends Serializabl
     // Todo add while logic to a separate method
     public Optional<V> get(@Nonnull K key) {
         Objects.requireNonNull(key, "key");
+        // Todo do I need to read it while holding a lock?
         while (true) {
             Object lock = locks.get(key);
             if (lock == null)
@@ -182,71 +131,6 @@ public class ConcurrentFileStorage<K extends Serializable, V extends Serializabl
                             }
                         });
             }
-        }
-    }
-
-    @Override
-    public boolean contains(@Nonnull K key) {
-        Objects.requireNonNull(key, "key");
-        return contents.containsKey(key);
-    }
-
-    @Override
-    public int getSize() {
-        return contents.size();
-    }
-
-    // Not thread-safe. Call with proper sync if needed
-    // Todo change to tryLock to avoid deadlocks? and add reaction to InterruptedException?
-    @SuppressWarnings("unchecked")
-    private Entry<K, V> deserialize(@Nonnull Path path) throws StorageException {
-        try (FileChannel channel = FileChannel.open(path);
-             ObjectInputStream ois = new ObjectInputStream(Channels.newInputStream(channel))) {
-            // create a shared lock to let other processes reading too
-            FileLock fileLock = channel.lock(0L, Long.MAX_VALUE, true);
-            try {
-                return (Entry) ois.readObject();
-            } finally {
-                fileLock.release();
-            }
-        } catch (Exception e) {
-            throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    // Not thread-safe. Call with proper sync if needed
-    // Todo thread-safe because it uses random path
-    private Path serialize(@Nonnull K key, @Nonnull V value) throws StorageException {
-        String filename = format(FILENAME_FORMAT, key.hashCode(), UUID.randomUUID().toString());
-        Path serialized = folder.resolve(filename);
-        boolean fileSaved = false;
-        try (FileOutputStream fos = new FileOutputStream(serialized.toFile());
-             FileChannel channel = fos.getChannel();
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            // exclusive lock access to the file by OS
-            FileLock fileLock = channel.lock();
-            try {
-                Entry<K, V> entry = new Entry<>(key, value);
-                oos.writeObject(entry);
-                fileSaved = true;
-                return serialized;
-            } finally {
-                fileLock.release();
-            }
-        } catch (Exception e) {
-            if (fileSaved)
-                throw new InconsistentFileStorageException(format("Exception after saving file %s has occurred", serialized), e, serialized);
-            else
-                throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    // Not thread-safe. Call with proper sync if needed
-    private boolean removeFile(@Nonnull Path path) throws StorageException {
-        try {
-            return Files.deleteIfExists(path);
-        } catch (Exception e) {
-            throw new RedundantFileStorageException("Couldn't remove file", e, path);
         }
     }
 
--- src/main/java/ru/glaizier/key/value/cache3/storage/file/ConfinedFileStorage.java
+++ src/main/java/ru/glaizier/key/value/cache3/storage/file/ConfinedFileStorage.java
@@ -1,96 +1,35 @@
 package ru.glaizier.key.value.cache3.storage.file;
 
-import static java.lang.String.format;
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toConcurrentMap;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.lang.invoke.MethodHandles;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileLock;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
-import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import ru.glaizier.key.value.cache3.storage.Storage;
 import ru.glaizier.key.value.cache3.storage.StorageException;
-import ru.glaizier.key.value.cache3.util.Entry;
 
 /**
  * Writes on disk are confined to one thread
  */
 @ThreadSafe
-public class ConfinedFileStorage<K extends Serializable, V extends Serializable> implements Storage<K, V> {
+public class ConfinedFileStorage<K extends Serializable, V extends Serializable> extends AbstractFileStorage<K, V> {
 
-    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
-    // filename format: <keyHash>-<uuid>.ser
-    private static final String FILENAME_FORMAT = "%d#%s.ser";
-
-    private static final Path TEMP_FOLDER = Paths.get(System.getProperty("java.io.tmpdir")).resolve("key-value-cache3");
-
-    private static final Pattern FILENAME_PATTERN = Pattern.compile("^(\\d+)#(\\S+)\\.(ser)$");
-
-    private final ConcurrentMap<K, Path> contents;
-
+    // Guards the folder and the contents-disk invariant
     private final ExecutorService diskWorker = Executors.newSingleThreadExecutor();
-
-    @GuardedBy("diskWorker")
-    private final Path folder;
 
     public ConfinedFileStorage() {
         this(TEMP_FOLDER);
     }
 
-    public ConfinedFileStorage(Path folder) {
-        Objects.requireNonNull(folder, "folder");
-        this.folder = folder;
-        try {
-            if (Files.notExists(folder)) {
-                Files.createDirectories(folder);
-            }
-            contents = buildContents(folder);
-        } catch (Exception e) {
-            throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    private ConcurrentMap<K, Path> buildContents(Path folder) throws IOException {
-        return Files.walk(folder)
-                .filter(Files::isRegularFile)
-                .filter(path -> FILENAME_PATTERN.matcher(path.getFileName().toString()).find())
-                .map(path -> {
-                    try {
-                        return new Entry<>(deserialize(path).key, path);
-                    } catch (Exception e) {
-                        log.error("Couldn't deserialize key-value for the path: " + path, e);
-                        return null;
-                    }
-                })
-                .filter(Objects::nonNull)
-                .collect(toConcurrentMap(entry -> entry.key, entry -> entry.value, (one, another) -> one));
+    public ConfinedFileStorage(@Nonnull Path folder) {
+        super(folder);
     }
 
     @Override
@@ -131,17 +70,6 @@ public class ConfinedFileStorage<K extends Serializable, V extends Serializable>
         return getFutureValue(task);
     }
 
-    @Override
-    public boolean contains(@Nonnull K key) {
-        Objects.requireNonNull(key, "key");
-        return contents.containsKey(key);
-    }
-
-    @Override
-    public int getSize() {
-        return contents.size();
-    }
-
     public boolean stopDiskWorker() throws InterruptedException {
         // Todo add shutdownNow()?
         diskWorker.shutdown();
@@ -155,73 +83,6 @@ public class ConfinedFileStorage<K extends Serializable, V extends Serializable>
             if (e.getCause() instanceof StorageException)
                 throw (StorageException) e.getCause();
             throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    private Optional<Entry<Path, V>> find(@Nonnull K key) {
-        Future<Optional<Entry<Path, V>>> task = diskWorker.submit(() -> {
-            if (contents.containsKey(key)) {
-                Path path = contents.get(key);
-                return of(new Entry<>(path, deserialize(path).value));
-            }
-            return empty();
-        });
-        try {
-            return task.get();
-        } catch (Exception e) {
-            throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    // Not thread-safe. Call with proper sync if needed
-    @SuppressWarnings("unchecked")
-    private Entry<K, V> deserialize(@Nonnull Path path) throws StorageException {
-        try (FileChannel channel = FileChannel.open(path);
-             ObjectInputStream ois = new ObjectInputStream(Channels.newInputStream(channel))) {
-            // create a shared lock to let other processes reading too
-            FileLock fileLock = channel.lock(0L, Long.MAX_VALUE, true);
-            try {
-                return (Entry) ois.readObject();
-            } finally {
-                fileLock.release();
-            }
-        } catch (Exception e) {
-            throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    // Not thread-safe. Call with proper sync if needed
-    private Path serialize(@Nonnull K key, @Nonnull V value) throws StorageException {
-        String filename = format(FILENAME_FORMAT, key.hashCode(), UUID.randomUUID().toString());
-        Path serialized = folder.resolve(filename);
-        boolean fileSaved = false;
-        try (FileOutputStream fos = new FileOutputStream(serialized.toFile());
-             FileChannel channel = fos.getChannel();
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            // exclusive lock access to the file by OS
-            FileLock fileLock = channel.lock();
-            try {
-                Entry<K, V> entry = new Entry<>(key, value);
-                oos.writeObject(entry);
-                fileSaved = true;
-                return serialized;
-            } finally {
-                fileLock.release();
-            }
-        } catch (Exception e) {
-            if (fileSaved)
-                throw new InconsistentFileStorageException(format("Exception after saving file %s has occurred", serialized), e, serialized);
-            else
-                throw new StorageException(e.getMessage(), e);
-        }
-    }
-
-    // Not thread-safe. Call with proper sync if needed
-    private boolean removeFile(@Nonnull Path path) throws StorageException {
-        try {
-            return Files.deleteIfExists(path);
-        } catch (Exception e) {
-            throw new RedundantFileStorageException("Couldn't remove file", e, path);
         }
     }
 
