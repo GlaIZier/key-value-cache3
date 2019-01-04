eb22599dfd3dfefc8ee473a6b9c31a53f56e80e8
GlaIZier <kh.mv@bk.ru>
2019-01-04 13:40:11 +0300

    SynchronizedMultiLevelCacheConcurrencyTest has been done

--- src/test/java/ru/glaizier/key/value/cache3/cache/SynchronizedMultiLevelCacheConcurrencyTest.java
+++ src/test/java/ru/glaizier/key/value/cache3/cache/SynchronizedMultiLevelCacheConcurrencyTest.java
@@ -24,7 +24,6 @@ import ru.glaizier.key.value.cache3.storage.memory.MemoryStorage;
 /**
  * @author GlaIZier
  */
-// Fixme
 public class SynchronizedMultiLevelCacheConcurrencyTest extends AbstractCacheConcurrencyTest {
 
     @Override
@@ -93,6 +92,76 @@ public class SynchronizedMultiLevelCacheConcurrencyTest extends AbstractCacheCon
 
     @Override
     public void evictPutRemove() throws InterruptedException, ExecutionException {
+        Cache<Integer, Integer> cache = getCache(THREADS_NUMBER * 2);
+        CyclicBarrier barrier = new CyclicBarrier(THREADS_NUMBER * 2);
+        // <THREADS_NUMBER> - <>
+        int initialCount = THREADS_NUMBER;
+        IntStream.range(0, initialCount).forEach(i -> cache.put(i, i));
 
+        AtomicInteger putWoEvictionCount = new AtomicInteger();
+        List<Callable<Object>> putEvictRemoveTasks = IntStream.range(THREADS_NUMBER, THREADS_NUMBER * 2)
+            .mapToObj(threadI -> (Runnable) () -> {
+                try {
+                    barrier.await(1, SECONDS);
+                    Thread.sleep((long) (Math.random() * 10));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                Thread.yield();
+                Optional<Map.Entry<Integer, Integer>> put = cache.put(threadI, threadI);
+                if (!put.isPresent())
+                    putWoEvictionCount.incrementAndGet();
+            })
+            .map(Executors::callable)
+            .collect(toList());
+        // + THREADS_NUMBER
+
+        AtomicInteger evictCount = new AtomicInteger();
+        List<Callable<Object>> evictTasks = IntStream.range(0, THREADS_NUMBER / 2)
+            .mapToObj(threadI -> (Runnable) () -> {
+                try {
+                    barrier.await(1, SECONDS);
+                    Thread.sleep((long) (Math.random() * 10));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                Thread.yield();
+                Optional<Map.Entry<Integer, Integer>> evict = cache.evict();
+                if (evict.isPresent())
+                    evictCount.incrementAndGet();
+            })
+            .map(Executors::callable)
+            .collect(toList());
+        // - evictCount
+
+        AtomicInteger removeCount = new AtomicInteger();
+        List<Callable<Object>> removeTasks = IntStream.range(THREADS_NUMBER / 2, THREADS_NUMBER)
+            .mapToObj(threadI -> (Runnable) () -> {
+                try {
+                    barrier.await(1, SECONDS);
+                    Thread.sleep((long) (Math.random() * 10));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                Thread.yield();
+                Optional<Integer> removed = cache.remove(threadI);
+                if (removed.isPresent())
+                    removeCount.incrementAndGet();
+            })
+            .map(Executors::callable)
+            .collect(toList());
+        // - removeCount
+
+        putEvictRemoveTasks.addAll(evictTasks);
+        putEvictRemoveTasks.addAll(removeTasks);
+        Collections.shuffle(putEvictRemoveTasks);
+        List<Future<Object>> futures = executorService.invokeAll(putEvictRemoveTasks);
+        // After all operations: from <5> - <5> (5 misses + 5 misses) to <> - <> (0 misses + 0 misses)
+
+        for (Future<Object> future : futures) {
+            future.get();
+        }
+
+        assertThat(cache.getSize(), is(initialCount + putWoEvictionCount.get() - removeCount.get() - evictCount.get()));
     }
 }
